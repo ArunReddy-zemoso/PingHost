d76dded3593c0f74eaccffb2c7f50df534c7350f
ArunZemoso <arunkumar.marella@zemosolabs.com>
2022-01-31 19:34:01 +0530

    init

---
+++ src/SystemCommand.java
@@ -0,0 +1,13 @@
+public class SystemCommand {
+    public static Process run(String IP){
+        Process process=null;
+        try {
+            String command="ping "+IP;
+            process=Runtime.getRuntime().exec(command);
+            return process;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return process;
+    }
+}
